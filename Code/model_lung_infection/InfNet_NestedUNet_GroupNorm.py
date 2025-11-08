7a18bc9332a538ab49dfff2fa74a1110ec5fd40f
ParthS007 <ParthS007@users.noreply.github.com>
2025-11-08 21:25:24 +0100

    fix: Update ReLU activation in VGGBlock to not use inplace operation for better gradient flow

--- Code/model_lung_infection/InfNet_NestedUNet_GroupNorm.py
+++ Code/model_lung_infection/InfNet_NestedUNet_GroupNorm.py
@@ -16,7 +16,7 @@ class VGGBlock(nn.Module):
 
     def __init__(self, in_channels, middle_channels, out_channels, num_groups=32):
         super().__init__()
-        self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.ReLU(inplace=False)
         self.conv1 = nn.Conv2d(in_channels, middle_channels, 3, padding=1)
         self.bn1 = nn.GroupNorm(num_groups, middle_channels)
         self.conv2 = nn.Conv2d(middle_channels, out_channels, 3, padding=1)
